e5e1a34246d8745745f67ab004f6ba86e9a368d6
Joshua Knight <joshuaknight@nevada.unr.edu>
2025-06-09 14:27:37 -0700

    Revert "migrations"
    This reverts commit dce17bc2d9ba5c78984d9b937881d352dc64fafb.

--- Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace url_shortener.Models;
-
-public class ApplicationUser : IdentityUser
-{
-    // You can add custom properties here if needed, e.g.,
-    public required string FullName { get; set; }
-}
